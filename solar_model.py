e6152501956ad74e63151cde81e709a0124e2213
Applopie <117065570+Applopie@users.noreply.github.com>
2022-12-17 03:19:43 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -1,15 +1,14 @@
 # coding: utf-8
 # license: GPLv3
 
+
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
-from math import atan2, cos, sin
+
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
@@ -19,11 +18,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        an = atan2(body.y - obj.y, body.x - obj.x)
-        F = -gravitational_constant * body.m * obj.m / (r**2)
-        body.Fx += F * cos(an)
-        body.Fy += F * sin(an)
-
+        body.Fx -= gravitational_constant * obj.m * body.m / r ** 3 * (body.x - obj.x)
+        body.Fy -= gravitational_constant * obj.m * body.m / r ** 3 * (body.y - obj.y)
 
 
 def move_space_object(body, dt):
@@ -32,19 +28,18 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить.
     """
 
-    ax = body.Fx/body.m
-    ay = body.Fy/body.m
+    ax = body.Fx / body.m
+    body.x += body.Vx * dt + ax * dt ** 2 / 2
     body.Vx += ax * dt
-    body.x += body.Vx*dt + 0.5 * ax * (dt**2)
+
+    ay = body.Fy / body.m
+    body.y += body.Vy * dt + ay * dt ** 2 / 2
     body.Vy += ay * dt
-    body.y += body.Vy*dt + 0.5 * ay * (dt**2)
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
